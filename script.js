147aac8239e709c22fe70845954a780dbeb7ed5b
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-23 23:42:07 +0100

    changes design

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const url = 'https://opentdb.com/api.php?amount=10&category=17&difficulty=medium&type=boolean'; //https://opentdb.com/api_config.php
-const questionsList = [];
+const url = 'https://opentdb.com/api.php?amount=10&category=17&difficulty=easy&type=boolean'; //https://opentdb.com/api_config.php
 
 const get = async (url) => {
   try {
@@ -9,17 +8,23 @@ const get = async (url) => {
   }catch (error) { console.error('Error fetching data', error) }
 };
 
-const showQuestions = async () => {
+const getQuestions = async () => {
+  const questionsList = [];
   const jsonObject = await get(url);
   const questions = await jsonObject.results;
   questions.forEach(e => {
     const qAndA = { question: null, answer: null };
     qAndA.question = e.question;
     qAndA.answer = e.correct_answer;
-    console.log(qAndA);
     questionsList.push(qAndA);
   });
+  showQuestion(questionsList, 1);
 }
 
-showQuestions();
-console.log(questionsList);
+const showQuestion = (questionsList, qIndex) => {
+  document.querySelector('#qIndex').innerHTML = `Q ${qIndex}/10`;
+  document.querySelector('#question').innerHTML = questionsList[qIndex-1].question;
+}
+
+getQuestions();
+
