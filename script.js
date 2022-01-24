f5e3caecb4394a3f166dd8a6aebf45702598db47
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-24 01:09:25 +0100

    implements scores and end condition

--- script.js
+++ script.js
@@ -9,40 +9,47 @@ const get = async (url) => {
 };
 
 const getQuestions = async () => {
-  const questionsList = [];
   const jsonObject = await get(url);
   const questions = await jsonObject.results;
-  questions.forEach(e => {
-    const qAndA = { question: null, answer: null };
-    qAndA.question = e.question;
-    qAndA.answer = e.correct_answer;
-    questionsList.push(qAndA);
-  });
-  showQuestion(questionsList, 1);
+  return questions;
 }
 
-const showQuestion = (questionsList, qIndex) => {
+const showQuestions = (questions, qIndex) => {
   document.querySelector('main').innerHTML = `
     <div class="row m-5 bg-dark text-light">
       <div id="qIndex" class="col m-3">Q ${qIndex}/10</div>
     </div>
     <div class="row my-5 p-5 text-center bg-warning">
-      <h3 id="question">${questionsList[qIndex-1].question}</h3>
+      <h3 id="question">${questions[qIndex-1].question}</h3>
     </div>
     <div class="row m-5 py-5 text-center justify-content-center">
       <button id="true" class="col-2 mx-4 p-2 btn btn-success">True</button>
       <button id="false" class="col-2 mx-4 p-2 btn btn-danger">False</button>
     </div>
   `;
-  createListener(questionsList, qIndex);
+
+  createListener(questions, qIndex);
 }
 
-const createListener = (questionsList, qindex) => {
+const createListener = (questions, qIndex) => {
+  if (qIndex+1 > 10) return;
   document.querySelector('#true').addEventListener('click', () => {
-    console.log('click');
-    showQuestion(questionsList, qindex+1)
+    if (questions[qIndex].correct_answer === 'True') score += 1;
+    console.log('SCORE: ' + score);
+    showQuestions(questions, qIndex+1);
+  });
+  document.querySelector('#false').addEventListener('click', () => {
+    if (questions[qIndex].correct_answer === 'False') score += 1;
+    console.log('SCORE: ' + score);
+    showQuestions(questions, qIndex+1);
   });
 }
 
-getQuestions();
+const game = async () => {
+  const questions = await getQuestions();
+  //console.log(questions);
+  showQuestions(questions, 1);
+}
 
+let score = 0;
+game();
